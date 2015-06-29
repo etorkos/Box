c9d3b4e4d65c46359687bf79d7e03f16d2ddf9d5
Evan Torkos <etorkos@gmail.com>
2015-06-29 18:03:12 -0400

    starting alerts with fix

--- app/models/rule.js
+++ app/models/rule.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose'),
   	Schema = mongoose.Schema;
 
-  	var RuleSchema = new Schema {
-  		targetSensor: {type: String. required: true},
+  	var RuleSchema = new Schema ({
+  		targetSensor: {type: String, required: true},
   		topBound: Number,
   		bottomBound: Number,
   		neededRate: Number,
-  	}
+  	});
 
   	mongoose.model('Rule', RuleSchema);
